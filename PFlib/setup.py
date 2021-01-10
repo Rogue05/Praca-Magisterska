9ccaafdabcc139e970a4d34244f3e0db70363430
Rogue05 <woso1997@gmail.com>
2021-01-10 22:53:06 +0100

    build sys

---
+++ PFlib/setup.py
@@ -0,0 +1,16 @@
+from glob import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+
+ext_modules = [
+    Pybind11Extension(
+        "PFlib",
+        sorted(glob("*.cpp")),  # Sort source files for reproducibility
+    ),
+]
+
+setup(
+    name="PFlib",
+    # extra_compile_args=['-O0','-Wall','-g'],
+    ext_modules=ext_modules,
+)
